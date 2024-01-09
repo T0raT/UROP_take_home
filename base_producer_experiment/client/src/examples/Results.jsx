f6a6c9524b908bf5acea49770f2783c23d6e1da7
T0raT <tigerliu@bu.edu>
2024-01-09 00:13:26 -0500

    Updated todo's and completed base feature for warrant. Still need to add different HTML elements for various cases, e.g. no warrant or product was not fraud.

--- base_producer_experiment/client/src/examples/Results.jsx
+++ base_producer_experiment/client/src/examples/Results.jsx
@@ -44,82 +44,128 @@ export function SalesResults({roundNumber}) {
 
   const numBuyers = Math.floor((Math.random() * (max - min ) + min)) ;
 
-  //Warrant RNG logic goes here
+  // Warrant RNG logic goes here
   /* 
-    TODO: Check if player is challenged, if true then also check if 
-    productionQuality === advertisementQuality.
-    If they aren't equal then player loses 90% of their current round income.
-
-    TODO 2: Different issue now, im thinking about changing salesScore and finalScore
-    by saving them into an object and dynamically updating them dependent on challenged condition.
+    TODO: Logic is done, need to check if the math is correct.
     */
   const salesScore = (numBuyers * (priceOfProduct - productionCost));
-  const finalScore = currentScore + salesScore;
+  const finalScore = currentScore + salesScore - WarrantPrice;
 
-  let newSalesScore = 0;
-  let newFinalScore = 0;
-  let WarrantChallenge = false;
-
-  if (Math.random() <= 0.3) {
-    WarrantChallenge = true;
+  let newSalesScore = salesScore;
+  let newFinalScore = finalScore;
+  let WarrantChallenge = Math.random() <= 0.9 && WarrantChoice; //High number for testing
+  if (WarrantChallenge) {
     console.log("Warrant challenge set to true.")
   }
 
   const challengeCond = WarrantChallenge && productionQuality === "low" && advertisementQuality === "high";
 
   if (challengeCond) {
-    newSalesScore = salesScore - Math.floor(salesScore * 0.9);
-    newFinalScore = currentScore + newSalesScore;
+    console.log("Warrant was challenged!")
+    newSalesScore = Math.floor(salesScore * 0.1);
+    newFinalScore = currentScore + newSalesScore - WarrantPrice;
     console.log("Warrant challenged. New sales score: ", newSalesScore);
     console.log("Warrant challenged. New final score: ", newFinalScore);
   }
 
 
 
-  //Calculates the final score for each round
-
-
 
   function handleSubmit() {
     console.log('Moving on from results round');
     player.stage.set("submit", true);
+    if (challengeCond) {player.set("score", newFinalScore);}
     player.set("score", finalScore);
   }
+
+  // TODO: Different HTML for different results dependent on warrant choice and challenge
+  // This is ugly as hell but it sure works!
   
-  return (
-    <div className="mt-3 sm:mt-5 p-20">
-      <h1 className="text-lg leading-6 font-medium text-gray-900">
-        Sales
-      </h1>
-      <div className="text-lg mt-2 mb-6">
-        {/* <p className="text-sm text-gray-500"> */}
-        <p>
-          You chose to produce a <b>{productionQuality}</b> quality product.
-        </p>
-        <p>
-          You chose to advertise it as a <b>{advertisementQuality}</b> quality product.
-        You sold it at a price of <b>${priceOfProduct}</b>.
-        <br /> <br />
-        </p>
+  if (challengeCond) {
+    return (
+      <div className="mt-3 sm:mt-5 p-20">
+        <h1 className="text-lg leading-6 font-medium text-gray-900">
+          Sales statistics below: 
+        </h1>
 
-        <img src={imageUrl} alt="Toothpaste Standard" width="250" height="250"/>
+        <div className="text-lg mt-2 mb-6">
+          {/* <p className="text-sm text-gray-500"> */}
+          <p>
+            You chose to produce a <b>{productionQuality}</b> quality product at the cost of <b>{productionCost}</b> points per unit sold.
+          </p>
+          <p>
+            You chose to advertise it as a <b>{advertisementQuality}</b> quality product.
+          You sold it at a price of <b>${priceOfProduct}</b>.
+          <br /> <br />
+          </p>
+  
+          <img src={imageUrl} alt="Toothpaste Standard" width="250" height="250"/>
+  
+          
+          <p>
+            It was advertised to an audience of 100 users, and {numBuyers} users bought your product.
+          </p>
+          <p> 
+            You earned ${priceOfProduct - productionCost}  per product x {numBuyers} units sold = {salesScore} points in sales.
+          </p><br/>
+          <p> Your score for this round is: {salesScore} </p>
+          <p>A score of {WarrantPrice} will be deducted because you chose to warrant your product</p>
+          <p> Your total score is: {finalScore} </p><br/>
 
-        
-        <p>
-          It was advertised to an audience of 100 users, and {numBuyers} users bought your product.
-        </p>
-        <p> 
-          You earned ${priceOfProduct - productionCost}  per product x {numBuyers} units sold = {salesScore} points in sales.
-        </p><br/>
-        <p> Your score for this round is: {salesScore} </p>
-        <p> Your total score is: {salesScore + currentScore} </p><br/>
-        <p> 
-          Click to proceed to the next round to sell products in this marketplace.
-        </p>
+          <p><b>But your warrant was challenged, and your product was found to be fraudulent, you lose 90% of your earnings for the current round.</b></p>
+          <br/>
+
+          <p> Your <b>new</b> score for this round is: {newSalesScore} </p>
+          <p>A score of {WarrantPrice} will be deducted because you chose to warrant your product</p>
+          <p> Your total score is: {newFinalScore} </p><br/>
+
+          <p> 
+            Click to proceed to the next round to sell products in this marketplace.
+          </p>
+        </div>
+        <Button handleClick={handleSubmit} primary>
+          I'm done!
+        </Button>
       </div>
-      <Button handleClick={handleSubmit} primary>
-        I'm done!
-      </Button>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="mt-3 sm:mt-5 p-20">
+        <h1 className="text-lg leading-6 font-medium text-gray-900">
+          Sales
+        </h1>
+        <div className="text-lg mt-2 mb-6">
+          {/* <p className="text-sm text-gray-500"> */}
+          <p>
+            You chose to produce a <b>{productionQuality}</b> quality product.
+          </p>
+          <p>
+            You chose to advertise it as a <b>{advertisementQuality}</b> quality product.
+          You sold it at a price of <b>${priceOfProduct}</b>.
+          <br /> <br />
+          </p>
+  
+          <img src={imageUrl} alt="Toothpaste Standard" width="250" height="250"/>
+  
+          
+          <p>
+            It was advertised to an audience of 100 users, and {numBuyers} users bought your product.
+          </p>
+          <p> 
+            You earned ${priceOfProduct - productionCost}  per product x {numBuyers} units sold = {salesScore} points in sales.
+          </p><br/>
+          <p> Your score for this round is: {salesScore} </p>
+          <p> Your total score is: {salesScore + currentScore} </p><br/>
+          <p> 
+            Click to proceed to the next round to sell products in this marketplace.
+          </p>
+        </div>
+        <Button handleClick={handleSubmit} primary>
+          I'm done!
+        </Button>
+      </div>
+    );
+  }
+
+
 }
